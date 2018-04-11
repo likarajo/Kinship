f5e0ae351010746e80f1df6749df7c182304f96b
Rajarshi Chattopadhyay <likarajo@gmail.com>
2018-04-11 21:14:59 +0530

    added javascript main.js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,12 @@
-function showDemo() {
-  _demo = document.getElementById("demo");
-  _demoLink = document.getElementById("demoLink");
-  
-}
+$(function() {
+	$('#demoLink').click(function() {
+		$('#demo').show();
+        $('#home').hide();
+        return false;
+    });
+	$('#homeLink').click(function() {
+        $('#demo').hide();
+        $('#home').show();
+        return false;
+    });
+});
