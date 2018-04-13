7000fd7aa45f54092b5dd11c3fc76e3c01c09672
Rajarshi Chattopadhyay <likarajo@gmail.com>
2018-04-13 13:13:49 +0530

    removed navigation and added transition

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,18 @@
-$(function() {
-	$('#demoLink').click(function() {
-		$('#demo').show();
-        $('#home').hide();
-        return false;
-    });
-	$('#homeLink').click(function() {
-        $('#demo').hide();
-        $('#home').show();
-        return false;
-    });
+//Using JQuery
+
+$(document).ready(function() {
+  $('#demoLink').click(function() {
+    $('#home').animate({ opacity: "-=1" }, 500, function() { $('#home').hide(); });
+    $('#demo').animate({ opacity: "+=1" }, 500, function() { $('#demo').show(); });
+    return false;
+  });
+});
+
+
+$(document).ready(function() {
+  $('#homeLink').click(function() {
+    $('#demo').animate({ opacity: "-=1" }, 500, function() { $('#demo').hide(); });
+    $('#home').animate({ opacity: "+=1" }, 500, function() { $('#home').show(); });
+    return false;
+  });
 });
